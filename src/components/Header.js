9e19c6408ebcd60e84460bc7f0731c36c2e08b2c
Joe Heiser <heiserj@gmail.com>
2020-10-12 13:44:40 -0700

    added a quality disclaimer

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import Nav from './Nav';
 
 const Header = () => (
   <header class='main-header'>
+    <p>Largely based on a rough data dump from Age of Empires 2 HD, in progress!</p>
     <h1>Age of Empires Quick Reference</h1>
     <Nav/>
   </header>
